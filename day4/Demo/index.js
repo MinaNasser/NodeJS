c7571b7765df10a5932868cf8b95f2d31771cc9e
MinaAl5al <minanasser82018@gmail.com>
2025-04-16 10:22:52 +0200

    Joi Validation

--- day4/Demo/index.js
+++ day4/Demo/index.js
@@ -1,7 +1,28 @@
 const express = require('express')
 const multer = require('multer')
+const DB = require('./DB');
+const Joi = require('joi')
+
 const app = express()
 const port = 3000;
+
+DB.ConnectDB();
+
+const Schema  =Joi.object({
+    name: Joi.string().required(),
+    age: Joi.number().required(),
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+    confirmPassword: Joi.string().required(),
+    avatar: Joi.string().required(),
+    gender: Joi.string().required(),
+    phone: Joi.string().required(),
+    address: Joi.string().required(),
+    role: Joi.string().required(),
+    status: Joi.string().required(),
+    createdAt: Joi.date().required(),
+    updatedAt: Joi.date().required(),
+})  
 
 
 app.use(express.json())
